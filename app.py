183084c0187c8d153520e01426213a4dd3a62f0f
Keerthi Gowda <88151540+hyperwolf24@users.noreply.github.com>
2025-02-10 16:44:21 +0530

    Added new pages. Added new image assets.

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/farm')
+def farm():
+    return render_template('farm.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == "__main__":    
     app.run(
         host='0.0.0.0',
         port=8080,
         debug=True,
-        )
+    )
