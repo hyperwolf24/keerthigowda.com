c533671861da5c1519df801a0762bd51c8d2c4c0
Keerthi Gowda <88151540+hyperwolf24@users.noreply.github.com>
2025-03-21 10:40:05 +0530

    Added few seo optimisations and improvements to all pages

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime, date
+import json
 
 app = Flask(__name__)
 
@@ -22,6 +23,41 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    host_url = request.host_url.rstrip('/')
+    today = date.today().strftime('%Y-%m-%d')
+    pages = [
+        {'loc': host_url + '/', 'lastmod': today},
+        {'loc': host_url + '/music', 'lastmod': today},
+        {'loc': host_url + '/farm', 'lastmod': today},
+        {'loc': host_url + '/about', 'lastmod': today},
+        {'loc': host_url + '/contact', 'lastmod': today},
+    ]
+    sitemap_xml = render_template('sitemap.xml', pages=pages)
+    response = app.response_class(sitemap_xml, mimetype='application/xml')
+    return response
+
+@app.route('/robots.txt')
+def robots():
+    return """
+User-agent: *
+Allow: /
+Sitemap: {}sitemap.xml
+""".format(request.host_url)
+
+@app.route('/.well-known/traffic-advice')
+def traffic_advice():
+    advice = {
+        "user-agent": "*",
+        "optimize-for-first-visit": True
+    }
+    return app.response_class(
+        response=json.dumps(advice),
+        status=200,
+        mimetype='application/json'
+    )
 
 if __name__ == "__main__":    
     app.run(
